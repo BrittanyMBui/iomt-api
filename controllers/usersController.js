98c42aeb13e4c8b18e5f6d656b9fa39b114dd43a
brittanymbui <brittymbui@gmail.com>
2021-03-01 20:52:05 -0800

    Adds login

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -5,9 +5,13 @@ const bcrypt = require('bcrypt');
 
 // - /user
 
-router.post('/signup', (req, res)=> {
-    bcrypt.genSalt(10, (err, salt)=>{
-        if (err) throw err;
+// USER SIGN-UP
+router.post('/signup', (req, res) => {
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+            console.log(err);
+        }
+
         bcrypt.hash(req.body.password, salt, (err, hashedPassword) => {
             const newUser = {
                 username: req.body.username,
@@ -24,5 +28,32 @@ router.post('/signup', (req, res)=> {
         })
     })
 });
+
+// USER LOGIN
+router.post('/login', (req, res) => {
+    db.User.findOne( { email: req.body.email }, (err, foundUser) => {
+        if (err) {
+            console.log(err)
+        }
+
+        if (!foundUser) {
+            return res.send('User does not exist');
+        }
+
+        bcrypt.compare(req.body.password, foundUser.password, (err, result) => {
+            if (err) {
+                console.log(err)
+            }
+
+            if (result) {
+                req.session.user = foundUser;
+                res.json(foundUser);
+            } else {
+                res.send('invalid password');
+            }
+        })
+    })
+})
+
 
 module.exports = router;
